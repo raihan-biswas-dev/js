2c6a212fb32472a8118a1f6885d52279e924cd2b
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2025-05-10 13:12:42 +0600

    Vanilla Js

---
+++ script.js
@@ -0,0 +1,137 @@
+// Javascript has 2 type of deta tye
+
+// 1. Primitive deta type (Number, String, Boolean, undefined, null)
+
+// Number
+// let x = 10;
+
+// // String
+// let y = "10";
+
+// // Boolean
+// let isTrue = true;
+// let isFalse = false;
+
+// // Undefined
+// let someThing;
+
+// // null
+// let kicuEkta = null;
+
+// 2. None Primitive deta type
+
+// let myName = "How" + " " + "are you?";
+
+// let welcomeMessage = "Hello" + " " + myName;
+
+// console.log(welcomeMessage);
+
+// let a = 12;
+// let b = 4;
+
+// let c = a + b;
+
+// document.getElementById("display").innerHTML = c;
+
+// let x = 6;
+// let y = 3;
+
+// // x += y; // X = X + Y
+
+// //x -= y; // X = X - Y
+
+// //x *= y; // X = X * Y
+
+// //x /= y; // X = X / Y
+
+// //x %= y; // X = X % Y
+// //x **= y; // X = X ** Y
+
+// x ^= y; // X = X ^ Y
+
+// document.write(x);
+
+/*
+let marks = 55;
+
+if (marks >= 80) {
+  document.write("A+");
+} else if (marks < 80 && marks >= 70) {
+  document.write("A");
+} else if (marks <= 70 && marks > 60) {
+  document.write("A-");
+} else if (marks <= 60 && marks > 50) {
+  document.write("B");
+} else if (marks <= 50 && marks > 40) {
+  document.write("B");
+} else if (marks <= 40 && marks >= 33) {
+  document.write("D");
+} else if (marks < 33) {
+  document.write("'F' You are fail");
+}
+
+*/
+
+/*
+
+let marks = 50;
+
+switch (true) {
+  case marks <= 100 && marks >= 80:
+    document.write("A+");
+    break;
+  case marks < 80 && marks >= 70:
+    document.write("A");
+    break;
+  case marks < 70 && marks >= 60:
+    document.write("A-");
+    break;
+  case marks < 60 && marks >= 50:
+    document.write("B");
+    break;
+  case marks < 50 && marks >= 40:
+    document.write("C");
+    break;
+  case marks < 40 && marks >= 33:
+    document.write("D");
+    break;
+
+  default:
+    document.write("F You are Fail");
+}
+
+*/
+
+// for loop  (==) Only compare value but (===) compare value and data type both
+
+/* let i;
+
+for (i = 0; i < 10; i++) {
+  if (i == 5 || i == 7) {
+    continue;
+  }
+  document.write(i + "<button>CLick Me!</button> </br>");
+}
+
+
+
+let i = 0;
+
+while (i < 10) {
+  document.write(i + "<button>CLick Me!</button> </br>");
+  i++;
+}
+
+//While loop
+
+
+let kotoBar = 0;
+
+while (kotoBar < 10) {
+  document.write(kotoBar + "kane Dhore Utbosh korar hoiace <br>");
+  kotoBar++;
+}
+
+*/
+
+
